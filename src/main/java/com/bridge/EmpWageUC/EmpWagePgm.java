7b1c4f7fc58599ff56ad09540ec85fa25c0ba2d2
Karthik <jeefkarthik036@gmail.com>
2022-03-23 23:26:14 +0530

    sixth-added UC4_Case

--- src/main/java/com/bridge/EmpWageUC/EmpWagePgm.java
+++ src/main/java/com/bridge/EmpWageUC/EmpWagePgm.java
@@ -1,25 +1,28 @@
 package com.bridge.EmpWageUC;
 
 public class EmpWagePgm {
+	public static final int IS_FULLTIME=1;
+	public static final int IS_PARTIME=2;
+	public static final int RATE_PER_HR=20;
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//CONSTANTS
-		int IS_FULLTIME=1;
-		int IS_PARTIME=2;
-		int RATE_PER_HR=20;
 		//Variables
 		int empHrs=0;
 		int empWage=0;
 		//Computation
-		double empCheck = Math.floor(Math.random()*10) % 3;
+		int empCheck = (int) Math.floor(Math.random()*10) % 3;
 		
-		if(empCheck == IS_FULLTIME)
-			empHrs=8;
-		else if(empCheck == IS_PARTIME)
-			empHrs=4;
-		else
-			empHrs=0;
+		switch (empCheck) {
+			case IS_FULLTIME:
+				empHrs=8;
+				break;
+			case IS_PARTIME:
+				empHrs=4;
+				break;
+			default:
+				empHrs=0;
+		}
 		
 		empWage = empHrs*RATE_PER_HR;
 		System.out.println("WAGE IS "+ empWage);
