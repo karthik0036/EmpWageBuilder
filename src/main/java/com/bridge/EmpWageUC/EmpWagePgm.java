2f6b3b894b951b500afdf0dcc2bdae7fcc25cd12
Karthik <jeefkarthik036@gmail.com>
2022-03-23 22:39:36 +0530

    fifth-added UC3 pgm

--- src/main/java/com/bridge/EmpWageUC/EmpWagePgm.java
+++ src/main/java/com/bridge/EmpWageUC/EmpWagePgm.java
@@ -6,15 +6,18 @@ public class EmpWagePgm {
 		// TODO Auto-generated method stub
 		//CONSTANTS
 		int IS_FULLTIME=1;
+		int IS_PARTIME=2;
 		int RATE_PER_HR=20;
 		//Variables
 		int empHrs=0;
 		int empWage=0;
 		//Computation
-		double empCheck = Math.floor(Math.random()*10) % 2;
+		double empCheck = Math.floor(Math.random()*10) % 3;
 		
 		if(empCheck == IS_FULLTIME)
 			empHrs=8;
+		else if(empCheck == IS_PARTIME)
+			empHrs=4;
 		else
 			empHrs=0;
 		
