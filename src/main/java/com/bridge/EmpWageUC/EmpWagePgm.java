cb1e8049df9c755c2fd9d5aa2838052768d33b64
Karthik <jeefkarthik036@gmail.com>
2022-03-23 22:21:50 +0530

    third-added UC2 Pgm

--- src/main/java/com/bridge/EmpWageUC/EmpWagePgm.java
+++ src/main/java/com/bridge/EmpWageUC/EmpWagePgm.java
@@ -4,13 +4,22 @@ public class EmpWagePgm {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//CONSTANTS
 		int IS_FULLTIME=1;
+		int RATE_PER_HR=20;
+		//Variables
+		int empHrs=0;
+		int empWage=0;
+		//Computation
 		double empCheck = Math.floor(Math.random()*10) % 2;
 		
 		if(empCheck == IS_FULLTIME)
-			System.out.println("Employee is present");
+			empHrs=8;
 		else
-			System.out.println("Employee is absent");
+			empHrs=4;
+		
+		empWage = empHrs*RATE_PER_HR;
+		System.out.println("WAGE IS "+ empWage);
 			
 	}
 
