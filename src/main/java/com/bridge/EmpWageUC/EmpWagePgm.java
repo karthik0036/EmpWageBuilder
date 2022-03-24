2a1fd9b140e916521ab672df16441704a545b456
Karthik <jeefkarthik036@gmail.com>
2022-03-24 07:37:03 +0530

    eigth-added UC-6

--- src/main/java/com/bridge/EmpWageUC/EmpWagePgm.java
+++ src/main/java/com/bridge/EmpWageUC/EmpWagePgm.java
@@ -5,13 +5,15 @@ public class EmpWagePgm {
 	public static final int IS_PARTIME=2;
 	public static final int RATE_PER_HR=20;
 	public static final int NUM_WORKING_DAYS=20;
+	public static final int MAX_HRS_MONTH=100;
 	
 
 	public static void main(String[] args) {
 		//Variables
-		int empHrs=0,empWage=0,totalWage=0;
+		int empHrs=0,empWage=0,totalEmpHrs=0,totalWorkingDays=0,totalWage=0;
 		//Computation
-		for(int day=0;day<NUM_WORKING_DAYS;day++) {
+		while(totalEmpHrs<=MAX_HRS_MONTH && totalWorkingDays<NUM_WORKING_DAYS) {
+			totalWorkingDays++;
 		int empCheck = (int) Math.floor(Math.random()*10) % 3;
 		
 		switch (empCheck) {
@@ -26,9 +28,11 @@ public class EmpWagePgm {
 		}
 		
 		empWage = empHrs*RATE_PER_HR;
-		System.out.println("WAGE IS "+ empWage);
-		totalWage+=empWage;
+		totalEmpHrs+=empHrs; 
+		System.out.println("DAY "+totalWorkingDays+" EmpHrs "+ empHrs+" wage is "+ empWage);
+		
 		}
+		totalWage=totalEmpHrs*RATE_PER_HR;
 		System.out.println("TOTALWAGE IS "+ totalWage);
 			
 	}
